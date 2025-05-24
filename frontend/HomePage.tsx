07dea41bd903a853c0951e3b1e66328cd281160a
Balmoral Sistemas de Informação <caio@balmoralsolucaoempresarial.com.br>
2025-05-24 00:20:36 +0000

    Estrutura React

---
+++ frontend/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="hero-container">
+      <h1>Balmoral - Plataforma PIC </h1>
+      <p>Simule, contrate e acesse as melhores linhas de crédito do Brasil.</p>
+      <a className="btn btn-warning" href="/cadastro">Começar agora!</a>
+    </div>
+  );
+}
